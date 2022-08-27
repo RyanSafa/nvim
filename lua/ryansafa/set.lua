e7f8800d0e1d621c31d3b354c6ce798a92eda45b
RyanSafa <ryansafa2@gmail.com>
2022-08-27 14:51:04 -0500

    change cmd height

--- lua/ryansafa/set.lua
+++ lua/ryansafa/set.lua
@@ -17,7 +17,7 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 -- Give more space for displaying messages.
-vim.opt.cmdheight = 2
+vim.opt.cmdheight = 1
 
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
