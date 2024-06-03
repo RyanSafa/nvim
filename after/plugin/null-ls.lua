3b265d5b659e9e47cabaf4273d9d229470d62cb1
RyanSafa <ryansafa2@gmail.com>
2024-06-02 23:54:04 -0400

    new commit idk

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -10,17 +10,4 @@ null_ls.setup({
 		formatting.rustfmt,
 		formatting.clang_format,
 	},
-	-- on_attach = function(client, bufnr)
-	-- 	if client.supports_method("textDocument/formatting") then
-	-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	-- 		vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 			group = augroup,
-	-- 			buffer = bufnr,
-	-- 			callback = function()
-	-- 				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-	-- 				vim.lsp.buf.format({ bufnr = bufnr })
-	-- 			end,
-	-- 		})
-	-- 	end
-	-- end,
 })
